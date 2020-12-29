1065749e55b582865d93544a02b982ffed0aece0
Bositkhon <bositkhon.sh@gmail.com>
2020-12-30 01:32:08 +0500

    added comments on each operation

--- server.c
+++ server.c
@@ -8,14 +8,15 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-#define BUFSIZE 2048
-int clientfd_set[100];
-char buffer[BUFSIZE];
-pthread_mutex_t mutex_buffer = PTHREAD_MUTEX_INITIALIZER;
-int read_buffer_ok;
-void *service_read(void*);
-void *service_deliver(void *data);
+#define BUFSIZE 2048 // максимальный размер буфера
+int clientfd_set[100]; // массив который будет хранить дескрипторы подключений клиентов
+char buffer[BUFSIZE]; // массив буфера
+pthread_mutex_t mutex_buffer = PTHREAD_MUTEX_INITIALIZER; // не знаю зачем она нужна, но не используется
+int read_buffer_ok; // не используется
+void *service_read(void*); // инициализация метода
+void *service_deliver(void *data); // инициализация метода
 
+// тип сообщения
 struct msg {
 	char *msg;
 };
@@ -25,49 +26,49 @@ int main(int argc, char *argv[])
 	int sockfd, clientfd, sockfd_len, client_len;
 	unsigned short port;
 	struct sockaddr_in server_addr, client_addr;
-	pthread_t thread_id[100];
+	pthread_t thread_id[100]; // массив дескрипторов тредов для подключенных клиентов
 	
+    // валидация аргументов 
 	if (argc < 2){
 		fprintf(stderr, "usage %s portnumber\n", argv[0]);
 		exit(0);
 	}
-	port = atoi(argv[1]);
+	port = atoi(argv[1]); // порт из аргумента программы
 	
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); // создание сокет дескриптора с настройками
+    // если не удалось создать сокет сервер
 	if (sockfd < 0) {
 		perror("Opening socket");
 		exit(0);
 	}
 
-	server_addr.sin_family = AF_INET;
+	server_addr.sin_family = AF_INET; // TCP
 	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(port);
+	server_addr.sin_port = htons(port); // обозначение порта
 
-	sockfd_len = sizeof(server_addr);
-	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+	sockfd_len = sizeof(server_addr); // размер адреса
+
+	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) { // создание сокета и присвоение порта
 		perror("Binding socket");
 		exit(0);
 	}
 	
-	if (listen(sockfd, 5) < 0) {
+	if (listen(sockfd, 5) < 0) { // начало прослушки
 		perror("Listening");
 		exit(0);
 	}
 
 	while (1) {
-		client_len = sizeof(client_addr);
-		clientfd = accept(sockfd,
-				  (struct sockaddr *)&client_addr,
-				  (socklen_t *)&client_len);
-		if (clientfd < 0) {
+		client_len = sizeof(client_addr); // размер адреса клиента
+		clientfd = accept(sockfd, (struct sockaddr *)&client_addr, (socklen_t *)&client_len); // ожидание подключения клиента
+		if (clientfd < 0) { // если не удалось подключиться
 			perror("Accpeting");
 		}
 		//fcntl(clientfd, F_SETFL, O_NONBLOCK | O_FSYNC);
-		for (int i = 0; i < 100; i++) {
-			if (clientfd_set[i] == 0) {
-				clientfd_set[i] = clientfd;
-				pthread_create(&thread_id[i], NULL,
-					       service_read, (void *)(long)i);
+		for (int i = 0; i < 100; i++) { // проходимся по массиву подключенных клиентов
+			if (clientfd_set[i] == 0) { // если есть подключенный
+				clientfd_set[i] = clientfd; // создаем "сессию" для него
+				pthread_create(&thread_id[i], NULL, service_read, (void *)(long)i); // создает отдельный процесс который запускает метод service_read которому передается переменная i
 				break;
 			}
 		}
@@ -77,23 +78,23 @@ int main(int argc, char *argv[])
 
 void *service_read(void *data)
 {
-	int i = (int)(long)data;
-	int clientfd = clientfd_set[i];
-	int n;
+	int i = (int)(long)data; // что передолось в этот метод переконвертируемся обратно на тип int чтобы последующие разы отдавать ответ нужному клиенту
+	int clientfd = clientfd_set[i]; // находим дескриптора клиента
+	int n; // для прочитанных битов
 	
 	printf("Thread created, fd = %d\n", clientfd);
 	while (1) {
 		//pthread_mutex_lock(&mutex_buffer);
-		n = recv(clientfd, buffer, BUFSIZE - 2, 0);
-		buffer[n] = '\0';
-		if (buffer[0] == 'e') {
-			close(clientfd);
-			clientfd_set[i] = 0;
-			pthread_exit(0);
+		n = recv(clientfd, buffer, BUFSIZE - 2, 0); // читаем что было отправлено от клиента
+		buffer[n] = '\0'; // символ конца строки 
+		if (buffer[0] == 'e') { // если сообщение начинается на букву 'e'
+			close(clientfd); // закрываем подключение
+			clientfd_set[i] = 0; // удаляем дескриптор
+			pthread_exit(0); // заканчиваем тред процесс
 		}
-		printf("%s\n", buffer);
+		printf("%s\n", buffer); // выводим в консоли сообщение 
 		fflush(stdout);
-		service_deliver(NULL);
+		service_deliver(NULL); // этот метод доставляет сообщение полученное от любого клиента всем клиентам
 	}
 	//pthread_mutex_unlock(&mutex_buffer);
 	return NULL;
@@ -103,10 +104,10 @@ void *service_deliver(void *data)
 {
 	// *data is of no use
 	//pthread_mutex_lock(&mutex_buffer);
-	for (int i = 0, fd = 0; i < 100; i++) {
-		if ((fd = clientfd_set[i]) > 0) {
-			printf("send to %d\n", fd);
-			send(fd, buffer, 98, 0);
+	for (int i = 0, fd = 0; i < 100; i++) { // проходится по массиву дескрипторов каждого клиента
+		if ((fd = clientfd_set[i]) > 0) { // если таковой есть, то есть подключеный
+			printf("send to %d\n", fd); // вывод
+			send(fd, buffer, 98, 0); // отправляет последнее полученное сообщеение в буфере клиенту, то есть грубо говоря всем
 		}
 	}
 	//pthread_mutex_unlock(&mutex_buffer);
